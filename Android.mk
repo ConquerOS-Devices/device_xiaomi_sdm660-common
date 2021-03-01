loserq/android_device_xiaomi_sdm660-common

sdm660-common: Add platina to the supported devices list
Change-Id: I63b18d20003ed9543801f5946c6a26140d1f3eef

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason lavender twolip wayne whyred,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout jason lavender platina twolip wayne whyred,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 

setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "jasmine_sprout jason lavender twolip wayne whyred"
+write_headers "jasmine_sprout jason lavender platina twolip wayne whyred"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

ConquerOS-Devices/device_xiaomi_sdm660-common

Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason lavender tulip wayne whyred,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout jason lavender platina tulip wayne whyred,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 

setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "jasmine_sprout jason lavender tulip wayne whyred"
+write_headers "jasmine_sprout jason lavender platina tulip wayne whyred"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
5c659ce1ccb1a96f570835e5368b4c5f8c00ace7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-03 16:34:16 +0100

    sdm660-common: Add platina to the supported devices list
    Change-Id: I63b18d20003ed9543801f5946c6a26140d1f3eef
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "jasmine_sprout jason lavender tulip wayne whyred"
+write_headers "jasmine_sprout jason lavender platina tulip wayne whyred"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
