a5ae2a50d7b089ec3bcc57ff12a4af6dfb819bdd
kerneltoast <sultan@kerneltoast.com>
2021-04-21 10:20:16 +0700

    sdm660-common: Force triple frame buffers
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- properties.mk
+++ properties.mk
@@ -133,6 +133,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=0 \
     ro.opengles.version=196610 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
